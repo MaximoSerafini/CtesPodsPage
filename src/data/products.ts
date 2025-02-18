7d5c8404ea33cab82abd980eb1b18499a3ec7c31
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-02-18 18:52:21 -0300

    actualizacion

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,12 +1,12 @@
 export const products = [
   {
     id: 1,
-    name: "WAKA 36000 GREEN APPLE",
+    name: "WAKA 36000 Peach Mango Watermelon",
     description: "36.000 Puff 5%",
     price: 21500,
-    image: "https://i.imgur.com/gwLYqxL.png",
+    image: "https://i.imgur.com/0PQKR7s.png",
     available: true,
-    stock: 10
+    stock: 5
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const products = [
     price: 21500,
     image: "https://i.imgur.com/svFUKz1.png",
     available: true,
-    stock: 10
+    stock: 5
   },
   {
     id: 3,
@@ -24,15 +24,60 @@ export const products = [
     price: 21500,
     image: "https://i.imgur.com/kTYFEOh.png",
     available: true,
-    stock: 10
+    stock: 5
   },
   {
     id: 4,
-    name: "WAKA 36000 ICY MINT",
+    name: "WAKA 36000 Watermelon",
     description: "36.000 Puff 5%",
     price: 21500,
-    image: "https://i.imgur.com/XEfwSzR.png",
+    image: "https://i.imgur.com/KBXONTi.png",
     available: true,
-    stock: 10
-  }
+    stock: 5
+  },
+  {
+    id: 5,
+    name: "ELFBAR 10000 Miami Mint",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/2LFrWiC.png",
+    available: true,
+    stock: 8
+  },
+  {
+    id: 6,
+    name: "ELFBAR 10000 Watermelon Ice",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/bRM6gCV.png",
+    available: true,
+    stock: 7
+  },
+  {
+    id: 7,
+    name: "ELFBAR 10000 Blue Razz Ice",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/JUksgwE.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 8,
+    name: "ELFBAR 10000 Mango Madness",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/CyKDvhI.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 9,
+    name: "ELFBAR 10000 Cherry Watermelon",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/fTPqdY7.png",
+    available: true,
+    stock: 5
+  },
 ];
