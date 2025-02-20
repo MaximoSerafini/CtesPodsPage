164d28efb948dba78eb87c07766787bddb8b50d8
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-02-20 19:08:34 -0300

    sinstock

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,83 +1,11 @@
 export const products = [
   {
-    id: 1,
-    name: "WAKA 36000 Strawberry Watermelon",
-    description: "36.000 Puff 5%",
-    price: 21500,
-    image: "https://i.imgur.com/tdWc4FW.png",
-    available: true,
-    stock: 5
-  },
-  {
-    id: 2,
-    name: "WAKA 36000 BLUE RAZZ",
-    description: "36.000 Puff 5%",
-    price: 21500,
-    image: "https://i.imgur.com/svFUKz1.png",
-    available: true,
-    stock: 5
-  },
-  {
-    id: 3,
-    name: "WAKA 36000 GRAPE",
-    description: "36.000 Puff 5%",
-    price: 21500,
-    image: "https://i.imgur.com/kTYFEOh.png",
-    available: true,
-    stock: 5
-  },
-  {
-    id: 4,
-    name: "WAKA 36000 Watermelon",
-    description: "36.000 Puff 5%",
-    price: 21500,
-    image: "https://i.imgur.com/KBXONTi.png",
-    available: true,
-    stock: 5
-  },
-  {
-    id: 5,
-    name: "ELFBAR 10000 Miami Mint",
-    description: "10.000 Puff 5%",
-    price: 12500,
-    image: "https://i.imgur.com/2LFrWiC.png",
-    available: true,
-    stock: 8
-  },
-  {
-    id: 6,
-    name: "ELFBAR 10000 Watermelon Ice",
-    description: "10.000 Puff 5%",
-    price: 12500,
-    image: "https://i.imgur.com/bRM6gCV.png",
-    available: true,
-    stock: 7
-  },
-  {
-    id: 7,
-    name: "ELFBAR 10000 Blue Razz Ice",
-    description: "10.000 Puff 5%",
-    price: 12500,
-    image: "https://i.imgur.com/JUksgwE.png",
-    available: true,
-    stock: 5
-  },
-  {
-    id: 8,
-    name: "ELFBAR 10000 Mango Madness",
-    description: "10.000 Puff 5%",
-    price: 12500,
-    image: "https://i.imgur.com/CyKDvhI.png",
-    available: true,
-    stock: 5
-  },
-  {
-    id: 9,
-    name: "ELFBAR 10000 Cherry Watermelon",
-    description: "10.000 Puff 5%",
-    price: 12500,
-    image: "https://i.imgur.com/fTPqdY7.png",
-    available: true,
-    stock: 5
+    //id: 1,
+    //name: "WAKA 36000 Strawberry Watermelon",
+    //description: "36.000 Puff 5%",
+    //7price: 21500,
+    //image: "https://i.imgur.com/tdWc4FW.png",
+    //available: true,
+    //stock: 5
   },
 ];
