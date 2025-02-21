cc47c3598faf79b226088021effa0bd666635c63
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-02-21 14:09:07 -0300

    sabores

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,11 +1,83 @@
 export const products = [
   {
-    //id: 1,
-    //name: "WAKA 36000 Strawberry Watermelon",
-    //description: "36.000 Puff 5%",
-    //7price: 21500,
-    //image: "https://i.imgur.com/tdWc4FW.png",
-    //available: true,
-    //stock: 5
+    id: 1,
+    name: "WAKA 36000 Blue Razz",
+    description: "36.000 Puff 5%",
+    price: 21500,
+    image: "https://i.imgur.com/OZywagQ.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 2,
+    name: "WAKA 25000 Grape Ice",
+    description: "25.000 Puff 5%",
+    price: 18500,
+    image: "https://i.imgur.com/7htzbQC.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 3,
+    name: "WAKA 25000 Watermelon Ice",
+    description: "25.000 Puff 5%",
+    price: 18500,
+    image: "https://i.imgur.com/7htzbQC.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 4,
+    name: "WAKA 25000 Strawberry Grape",
+    description: "25.000 Puff 5%",
+    price: 18500,
+    image: "https://i.imgur.com/7htzbQC.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 5,
+    name: "ElfBar 10000 Miami Mint",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/J8IXwjG.png",
+    available: true,
+    stock: 8
+  },
+  {
+    id: 6,
+    name: "ElfBar 10000 Blue Razz Ice",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/x78X7xb.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 7,
+    name: "ElfBar 10000 Mango Madness",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/eEZMkCc.png",
+    available: true,
+    stock: 5
+  },
+  {
+    id: 8,
+    name: "ElfBar 10000 Watermelon Ice",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/C1aLzy0.png",
+    available: true,
+    stock: 7
+  },
+  {
+    id: 8,
+    name: "ElfBar 10000 Cherry Lemon",
+    description: "10.000 Puff 5%",
+    price: 12500,
+    image: "https://i.imgur.com/486erne.png",
+    available: true,
+    stock: 5
   },
 ];
