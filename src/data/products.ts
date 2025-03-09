e9b10092fb9bcb1def6af16e250278f02e5f1821
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-03-09 17:56:50 -0300

    stock actua

--- src/data/products.ts
+++ src/data/products.ts
@@ -32,8 +32,8 @@ export const products = [
     description: "Sabor a fresa y uva con efecto Ice y ofrece hasta 25,000 puffs con 5% de nicotina.",
     price: 18500,
     image: "https://i.imgur.com/7htzbQC.png",
-    available: true,
-    stock: 1
+    available: false,
+    stock: 0
   },
   {
     id: 5,
