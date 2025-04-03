3472adf0c7440a0f8c7fc6a053c89e2b06d66e30
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-04-03 14:56:31 -0300

    Cambio descripcion productos

--- src/data/products.ts
+++ src/data/products.ts
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 1,
     name: "WAKA BLAST 36000",
-    description: "36.000 Puff 5% Nicotina",
+    description: "36.000 Pitadas 5% Nicotina",
     price: 20000,
     available: true,
     stock: 5,
@@ -28,7 +28,7 @@ export const products = [
   {
     id: 2,
     name: "ELFBAR 10000",
-    description: "10.000 Puff 5% Nicotina",
+    description: "10.000 Pitadas 5% Nicotina",
     price: 11000,
     available: true,
     stock: 8,
@@ -58,7 +58,7 @@ export const products = [
   {
     id: 3,
     name: "ELFBAR TOUCH 10000",
-    description: "10.000 Puff 5% Nicotina",
+    description: "10.000 Pitadas 5% Nicotina",
     price: 11000,
     available: true,
     stock: 6,
